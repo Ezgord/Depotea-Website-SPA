600fa911fec59f7dc78342f24c644112dd889515
Ezgord <75807910+Ezgord@users.noreply.github.com>
2022-04-13 03:27:21 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -34,8 +34,6 @@ var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
 		document.getElementById("index-body-container").innerHTML = this.responseText;
-		
-		linktoRiceMeals();
 	}
 };
 
