cf2ef8817fcecdbb842880ff06ef67044fbf6d70
Ezgord <75807910+Ezgord@users.noreply.github.com>
2022-04-15 01:44:24 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,9 +22,7 @@ var xhttp = new XMLHttpRequest();
 
 xhttp.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
-		document.getElementById("index-body-container").innerHTML = this.responseText;
-		
-		loadCheckout();
+		document.getElementById("index-body-container").innerHTML = this.responseText
 	}
 };
 
